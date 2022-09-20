d24001370e4ef63d529ff354aaefb7f17472b692
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-09-20 09:30:22 +0000

    テーブル編集３

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_090848) do
+ActiveRecord::Schema.define(version: 2022_09_20_092205) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
