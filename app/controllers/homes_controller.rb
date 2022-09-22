6dd82d2e3dada7c6499703d8cc328cd7cb764054
jodai <jodai0423@gmail.com>
2022-09-22 14:22:16 +0000

    top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
     def top
+        @products = Product.limit(4).order(" created_at DESC ")
     end
 end
--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,2 +1,19 @@
 class Public::CartProductsController < ApplicationController
+  
+  def index
+    @cart_products = CartProduct
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def all_destroy
+  end
+  
+  def create
+  end
+  
 end
