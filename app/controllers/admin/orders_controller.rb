ea69f846de468240ed51b38d4edf5b47f537a3aa
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-09-22 13:51:40 +0000

    管理者　ヘッダー

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,29 @@
 class OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @customer = @order.customers
+    @order_products = @order.order_products
+    @total = 0
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+
+    # [:order][:status]→orderの中のstatus
+    if params[:order][:status] == "入金確認"
+      # making_statusを製作待ちに更新
+      @order.order_product.update(making_status: 1)
+    end
+
+    redirect_to request.referer
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
