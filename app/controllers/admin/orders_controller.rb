bb24b2550f8785f69e30be47848689b63955eb4b
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-09-22 07:53:55 +0000

    add admin top

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,8 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @order_products = @order.products
+    @customer = @order.customers
+  end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,20 @@
 class Admin::ProductsController < ApplicationController
 
-  def index
-    @products = Product.page(params[:page])
+  def new
+        @product = Product.new
+        @genres = Genre.all
+  end
+
+  def create
+      @product = Product.new(product_params)
+      @product.admin_id = current_admin.id
+    if product.save
+      @product = Product.new
+      render 'new'
+      #仮設定
+    else
+      @product = Product.new
+      render 'new'
+    end
   end
 end
