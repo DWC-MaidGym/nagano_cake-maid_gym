a5ea8a413691f0a1cfc1a7d3caf245ff2de78130
rjaimasahide <110381931+rjaimasahide@users.noreply.github.com>
2022-09-22 17:04:58 +0900

    Delete orders_controller.rb

--- app/controllers/admin/orders_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::OrdersController < ApplicationController
-
-  def show
-    @order = Order.find(params[:id])
-    @order_products = @order.products
-    @customer = @order.customers
-  end
-end
