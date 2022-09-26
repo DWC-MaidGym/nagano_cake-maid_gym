7ebb2f292dc8538a9328eadefa93c5673c2b0d88
EC2 Default User <ec2-user@ip-172-31-8-204.ap-northeast-1.compute.internal>
2022-09-26 12:07:17 +0000

    退会画面まではいける状態

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,10 +14,10 @@ class Public::CustomersController < ApplicationController
   end
   
   def withdraw
-    @customer = Customer.find(current_customer.id)
-    @customer.update(is_deleted: true)
-    reset_session
-    redirect_to root_path
+  @customer = Customer.find(current_customer.id)
+  @customer.update(is_deleted: true)
+  reset_session
+  redirect_to customer_withdraw_path
   end
   
   def create
