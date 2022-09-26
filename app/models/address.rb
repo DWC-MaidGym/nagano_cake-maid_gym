2d3bdac55c3f4b6fead046c918cd52d0032cfff7
EC2 Default User <ec2-user@ip-172-31-8-204.ap-northeast-1.compute.internal>
2022-09-26 05:14:23 +0000

    レイアウト調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,9 @@
 class Address < ApplicationRecord
+    
+  belongs_to :customer
 
-    belongs_to :customer, dependent: :destroy
-    
-    def address_display
-      '〒' + post_code + ' ' + address + ' ' + name 
-    end
-    
-    def full_adresses
+  def full_adresses
       post_code + address + name
-  　　　　end
-    
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,17 +72,17 @@ ActiveRecord::Schema.define(version: 2022_09_23_153645) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "telephone_number", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "is_deleted", default: false, null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "telephone_number"
-    t.string "post_code"
-    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
